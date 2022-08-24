69f7ea89fb0d734e18eeea19b186230e3b2e21ce
Andreas <Andreas@DESKTOP-TV3SA0P>
2022-08-24 17:02:28 +0200

    Add project files.

---
+++ DapperMinimalAPI_DB/dbo/StoredProcedures/spUser_GetAll.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetAll]
+
+AS
+begin
+
+	select * 
+	from dbo.[User];
+
+end
---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccess.DbAccess;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Dafault");
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Dafault");
+}
