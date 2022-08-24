93b8b9d17f54ab9b153d2fde6fad047f87140e85
Andreas <Andreas@DESKTOP-TV3SA0P>
2022-08-24 17:02:28 +0200

    Update

---
+++ DapperMinimalAPI_DB/Script.PostDeployment.sql
@@ -0,0 +1,11 @@
+﻿if not exists (select 1 from dbo.[User])
+
+begin
+
+	insert into dbo.[User] (FirstName, LastName)
+	values ('Test1', 'User1'),
+	('Test2', 'User2'),
+	('Test3', 'User3'),
+	('Test4', 'User4');
+
+end
